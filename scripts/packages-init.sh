4a9ed8c52c7c22d8d08c8ae5fc06a8176eb5fbc0
Brandon Grant <grantb09@gmail.com>
2023-11-28 16:21:11 +0000

    Add `packages-init`

---
+++ scripts/packages-init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# update apt sources and install nala
+sudo apt update && sudo apt install -y nala
+
+# update pacakge library and current packages
+sudo nala upgrade -y
+
+# install terminal packages
+sudo nala install -y \
+zsh neofetch \
+python3-dev python3-pip python3-setuptools \
+
+# install spaceship-prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# install starship
+curl -sS https://starship.rs/install.sh | sh
+
+# install thefuck
+pip3 install thefuck --user
