092c80e060ea0d9b0cef05f6497800bfcd836847
Brandon Grant <grantb09@gmail.com>
2023-11-28 15:48:04 +0000

    Add `init.sh`

---
+++ scripts/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install packages
+curl https://github.com/Artiizan/dotfiles/blob/main/scripts/packages-init.sh | bash
+
+# Configuration setup
+curl https://github.com/Artiizan/dotfiles/blob/main/scripts/config-init.sh | bash
