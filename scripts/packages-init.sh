639ac9f0034347f4e1773efaf95ab976ccec9269
Brandon Grant <grantb09@gmail.com>
2023-11-29 15:32:40 +0000

    Update packages-init.sh

--- scripts/packages-init.sh
+++ scripts/packages-init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # update apt sources and install nala
-echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 sudo apt update && sudo apt install -y nala
 
 # update pacakge library and current packages
