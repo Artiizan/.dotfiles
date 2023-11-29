1921c99e676ed7e8b7a48882caab288e57fa7f34
Brandon Grant <grantb09@gmail.com>
2023-11-29 15:12:11 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 # Install packages
 # Make sure to double check the contents of the curl are what you expect before running with `sudo`
-curl https://github.com/Artiizan/dotfiles/blob/main/scripts/packages-init.sh | sudo bash
+curl https://raw.githubusercontent.com/Artiizan/dotfiles/main/scripts/packages-init.sh | sudo bash
 
 # Configuration setup
-curl https://github.com/Artiizan/dotfiles/blob/main/scripts/config-init.sh | bash
+curl https://raw.githubusercontent.com/Artiizan/dotfiles/main/scripts/config-init.sh | bash
