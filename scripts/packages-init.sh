7006ce262981a4683e927341dfd43470f029c944
Brandon Grant <grantb09@gmail.com>
2023-12-01 07:59:52 +0000

    Elevate privileges for `spaceship-prompt` installation

--- scripts/packages-init.sh
+++ scripts/packages-init.sh
@@ -19,11 +19,11 @@ zsh neofetch fonts-firacode \
 python3-dev python3-pip python3-setuptools
 
 # install spaceship-prompt
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh -y
+curl -sS https://starship.rs/install.sh | sh
 
 # install autosuggestions for zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
