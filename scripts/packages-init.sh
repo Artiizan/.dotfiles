25a801404450a42b590a2aba4dc315f2a13733c9
Brandon Grant <grantb09@gmail.com>
2023-12-01 08:19:28 +0000

    Install `thefuck` with `apt`

--- scripts/packages-init.sh
+++ scripts/packages-init.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt install -y nala
 sudo nala upgrade -y
 
 sudo nala install -y \
-git micro btop
+git micro btop thefuck
 
 ##### Terminal setup #####
 
@@ -15,8 +15,7 @@ sudo add-apt-repository universe
 
 # install terminal packages
 sudo nala install -y \
-zsh neofetch fonts-firacode \
-python3-dev python3-pip python3-setuptools
+zsh neofetch fonts-firacode
 
 # install spaceship-prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
@@ -27,8 +26,5 @@ curl -sS https://starship.rs/install.sh | sh
 
 # install autosuggestions for zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# install thefuck
-pip3 install thefuck --user
 
 chsh -s $(which zsh)
