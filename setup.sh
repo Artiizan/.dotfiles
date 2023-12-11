2863e72a308b8003146a8102a16d4d7a5c523be4
Brandon Grant <grantb09@gmail.com>
2023-12-11 11:01:15 +0000

    Simplify terraform install

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # Install for dockerized development environments
 
-sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install -y gpg
+sudo apt install -y gnupg software-properties-common
 
 # terraform
-curl https://apt.releases.hashicorp.com/gpg | gpg — dearmor > hashicorp.gpg
-sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
-sudo apt-add-repository “deb [arch=$(dpkg — print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
 
-sudo apt install -y \
-software-properties-common gnupg2 python3 \
-terraform
+sudo apt install -y terraform
 
 # ansible
+sudo apt install -y python3
 python3 -m pip install --user ansible
