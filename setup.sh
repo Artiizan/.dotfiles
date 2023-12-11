14909d15a8a503a79b9e9171ca5e98eb9dfd8b2c
Brandon Grant <grantb09@gmail.com>
2023-12-11 10:58:48 +0000

    Simplify

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 # Install for dockerized development environments
 
-# preseed for tzdata
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install tzdata -y --no-install-recommends
-
-# install development dependencies
+sudo apt install -y gpg
 
 # terraform
 curl https://apt.releases.hashicorp.com/gpg | gpg — dearmor > hashicorp.gpg
 sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository “deb [arch=$(dpkg — print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
 
 sudo apt install -y \
 software-properties-common gnupg2 python3 \
