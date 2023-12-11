b740ce6f1f3cf8636cc09b4b6ecbf39d8422d1b8
Brandon Grant <grantb09@gmail.com>
2023-12-11 11:09:47 +0000

    Add `DEBIAN_FRONTEND="noninteractive"`

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 # Install for dockerized development environments
 
 sudo apt update
-sudo apt install -y --no-install-recommends gnupg software-properties-common wget
+sudo DEBIAN_FRONTEND="noninteractive" apt install -y --no-install-recommends gnupg software-properties-common wget
 
 # terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
-sudo apt install -y terraform
+sudo DEBIAN_FRONTEND="noninteractive" apt install -y terraform
 
 # ansible
 python3 -m pip install --user ansible
