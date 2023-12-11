a16919525432c94b6428450e195aca3f4b736a3e
Brandon Grant <grantb09@gmail.com>
2023-12-11 15:18:48 +0000

    Comment out terraform install

--- setup.sh
+++ setup.sh
@@ -19,14 +19,6 @@ python3 -m pip install --user yamllint
 python3 -m pip install --user ansible-lint
 python3 -m pip install --user jmespath
 
-#################
-### terraform ###
-#################
-sudo apt install -y software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y terraform
-
 ##################
 ### kubernetes ###
 ##################
@@ -46,3 +38,11 @@ curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sud
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 sudo apt update && sudo apt install 1password-cli
+
+#################
+### terraform ###
+#################
+#sudo apt install -y software-properties-common
+#wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+#sudo apt update && sudo apt install -y terraform
